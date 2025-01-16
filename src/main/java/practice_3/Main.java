e49dc7ad4e5af49dc187e9ba229614cb1869b34f
alex.pshe <alexpshe.help@gmail.com>
2025-01-16 09:17:47 -0400

    Add practice_3

---
+++ src/main/java/practice_3/Main.java
@@ -0,0 +1,23 @@
+package practice_3;
+
+public class Main {
+    public static void main(String[] args) {
+        // создали переменную
+        // присвоили переменной экземпляр класса Студент
+        // вызывав дефолтный конструктор
+
+        System.out.println(Student.studentCount);
+
+        Student petya = new Student(18, "Петя");
+
+        int k = petya.age;
+
+        System.out.println(Student.studentCount);
+
+        Student kolya = new Student(20, "Коля");
+
+        System.out.println(Student.studentCount);
+
+        Student.printMaxYears();
+    }
+}
