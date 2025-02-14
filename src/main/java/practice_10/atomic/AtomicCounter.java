049c082281286e55862d5c49e1bcc926e8d0c9aa
alex.pshe <alexpshe.help@gmail.com>
2025-02-14 12:12:41 -0400

    Add practice_10

---
+++ src/main/java/practice_10/atomic/AtomicCounter.java
@@ -0,0 +1,8 @@
+package practice_10.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicCounter {
+    // считает количество операций по всем потокам
+    public static final AtomicInteger count = new AtomicInteger(0);
+}
